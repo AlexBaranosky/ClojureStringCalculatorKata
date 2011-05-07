2ad26eb79cdab5c8ddf54d065d4cb5096c23e282
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-07 15:50:42 -0400

    one adds up to one

--- stringcalculator.clj
+++ stringcalculator.clj
@@ -1,7 +1,11 @@
-(defn add [s] 0) 
+(defn add [s]
+  (case s
+    "" 0
+    1))
 
-(defn assert-equals [msg expressionA expressionB] 
-	(let [result (cond (= expressionA expressionB) "yes" :else "you failed")]
-		(println (str result ": " msg))))		
-		
+(defn assert-equals [msg expressionA expressionB]
+	(let [result (cond (= expressionA expressionB) "yes: " :else "you failed: ")]
+		(println (str result msg))))
+
 (assert-equals "blank string adds up to 0" 0 (add ""))
+(assert-equals "one adds up to one" 1 (add "1"))
