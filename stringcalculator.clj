7cac5d49e58a9423726be4d397aa88cc510578ce
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-07 16:23:05 -0400

    can specify my own delimiter

--- stringcalculator.clj
+++ stringcalculator.clj
@@ -1,9 +1,13 @@
-(def sum (partial reduce +))
+(defn delimiter+number-str [s]
+  (cond
+    (.startsWith s "//;") {:delimiter (.substring s 2 3) :number-str (.substring s 4) }
+    (= s "")              {:delimiter "," :number-str "0"}
+    :else                 {:delimiter "," :number-str s}))
 
 (defn add [s]
-  (cond
-    (= s "") 0
-    :else (sum (map #(Integer/parseInt %) (.split s ",|\n")))))
+  (let [{:keys [delimiter number-str]} (delimiter+number-str s)
+        numbers (map #(Integer/parseInt %) (.split number-str (str delimiter "|\n")))]
+    (reduce + numbers)))
 
 (defn assert-equals [msg expected actual]
 	(let [result (cond (= expected actual) "yes: " :else "you failed: ")]
@@ -14,3 +18,4 @@
 (assert-equals "one number alone adds up to itself" 2 (add "2"))
 (assert-equals "adds any number of comma-separated numbers" 20 (add "2,3,4,5,6"))
 (assert-equals "can separate numbers by either ',' or '\n'" 9 (add "2\n3,4"))
+(assert-equals "can specify your own delimiter" 3 (add "//;\n1;2"))
