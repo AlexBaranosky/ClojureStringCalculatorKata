28297d89b34dda9bddf4f5992b8f5d9a6320bd0b
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-07 16:06:23 -0400

    adds any number of comma separated numbers

--- stringcalculator.clj
+++ stringcalculator.clj
@@ -1,12 +1,15 @@
-(defn add [s]
-  (case s
-    "" 0
-    (Integer/parseInt s)))
+(def sum (partial reduce +))
 
-(defn assert-equals [msg expressionA expressionB]
-	(let [result (cond (= expressionA expressionB) "yes: " :else "you failed: ")]
+(defn add [s]
+  (cond
+    (= s "") 0
+    :else (sum (map #(Integer/parseInt %) (.split s ",")))))
+
+(defn assert-equals [msg expected actual]
+	(let [result (cond (= expected actual) "yes: " :else "you failed: ")]
 		(println (str result msg))))
 
 (assert-equals "blank string adds up to 0" 0 (add ""))
 (assert-equals "one adds up to one" 1 (add "1"))
 (assert-equals "one number alone adds up to itself" 2 (add "2"))
+(assert-equals "adds any number of comma-separated numbers" 20 (add "2,3,4,5,6"))
